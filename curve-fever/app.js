868468cf077bb17c6180faed0c8a8b7f8976d2bf
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2022-02-17 08:23:09 +0100

    Gaps added to Curve Fever and Bloom Effect added

--- curve-fever/app.js
+++ curve-fever/app.js
@@ -3,19 +3,26 @@ console.log('%cCurve Fever V0.1', 'background: #000; color: #ccc ; padding: 5px;
 var canvas = undefined;
 var speed = 1;
 var angle = 2;
+var bloom = false;
 var dimension = document.querySelector('#gameArea').clientWidth;
 var started = false;
 var players = [];
 var keys =['LEFT_ARROW','RIGHT_ARROW','A','D','V','B'];
 var keyCodes = [[37,39],[65,68],[86,66]];
-var playerColors = ["red","blue","green"];
+var playerColors = [["#000","fff"],"blue","green"];
 var lineWidth = 15;
 var parentContainerId = 'gameArea';
+var currentFromColor = undefined;
+var currentToColor = undefined;
+
+p5.disableFriendlyErrors = false;
 
 $(document).ready(function(){
+
     var speedInput = document.querySelector("#speed-input");
     var angleInput = document.querySelector("#angle-input");
     var lineWidthInput = document.querySelector("#line-width-input");
+    var bloomInput = document.querySelector("#bloom-input");
 
     // add listeners to settings
     speedInput.addEventListener('input',(e)=>{
@@ -27,14 +34,46 @@ $(document).ready(function(){
     lineWidthInput.addEventListener('input',(e)=>{
         lineWidth = parseInt(lineWidthInput.value);
     });
+    bloomInput.addEventListener('change',(e)=>{
+        bloom = bloomInput.checked;
+    });
 });
 
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+}
+
 function setup(){
+    currentFromColor = color(242, 64, 64);
+    currentToColor = color(86, 157, 252);
+
+    var gradientStartInput = document.querySelector("#gradient-start-input");
+    var gradientEndInput = document.querySelector("#gradient-end-input");
+
+    gradientStartInput.addEventListener('input',(e)=>{
+        var val = hexToRgb(gradientStartInput.value);
+        currentFromColor = color(val.r,val.g,val.b);
+        console.log(hexToRgb(gradientStartInput.value));
+    });
+    gradientEndInput.addEventListener('input',(e)=>{
+        var val = hexToRgb(gradientEndInput.value);
+        currentToColor = color(val.r,val.g,val.b);
+        console.log(hexToRgb(gradientEndInput.value));
+    });
     // create canvas
     canvas=createCanvas(dimension, dimension);
-    colorMode(HSB);
+    colorMode(RGB);
     background('black');
     canvas.parent(parentContainerId);
+}
+
+function preload(){
+    
 }
 
 function start(playerNumber){
@@ -51,6 +90,7 @@ function start(playerNumber){
             x = Math.floor(Math.random() * 0.8*dimension) + 1;
             line_tmp=new Line(x,x);
             line_tmp.add_position(x+speed,x+speed);
+            line_tmp.currentColor = color(Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255));
             players.push(line_tmp);
         }
         // set started bool
@@ -62,12 +102,18 @@ function draw(){
     // only draw if game is started
     if (started)
     {
+        //clear();
+        //background(0);
         for (var i=0;i<players.length;i++)
         {
+            players[i].resetHeadEnvironment();
             // check for key presses
             if (keyIsDown(keyCodes[i][0])) players[i].move(-angle* Math.PI / 180);
             else if (keyIsDown(keyCodes[i][1])) players[i].move(angle* Math.PI / 180);
             else players[i].move(0);
+
+            players[i].resetHeadEnvironment();
+            players[i].display_lines(players[i]);
         }
         
         // get looser id
@@ -77,6 +123,7 @@ function draw(){
         {
             started=false;
         }
-        Line.display_lines(players);
     }
+
+    $(".fps").html("FPS: " + Math.floor(frameRate()));
 }
--- curve-fever/curve_fever.js
+++ curve-fever/curve_fever.js
@@ -1,75 +1,108 @@
 class Line{
-    static interruption = false;
-    static interruptionInterval = undefined;
 
     constructor(a,b){
       this.array=[];
       this.array.push([a,b]);
+      this.head = [a,b];
+      this.interruption = false;
+      this.currentColor = currentFromColor;
+      this.colorCounter = 0;
+      this.interruptionInterval = setInterval(this.randomInterval.bind(this), Math.random() * 1000 + 1000);
+      this.colorCounterMode = "increment";
     }
 
-    static randomInterval(){
-        Line.interruption = true;
+    removeInterruption(){
+        this.interruption = false;
+    }
+
+    randomInterval(){
+        this.interruption = true;
         var rndDuration = Math.floor((Math.random() * 500 + 250));
-        setTimeout(() => {
-            Line.removeInterruption();
-        }, rndDuration);
-    }
-
-    static removeInterruption(){
-        Line.interruption = false;
+        setTimeout(this.removeInterruption.bind(this), rndDuration);
     }
   
     add_position(a,b){
-      this.array.push([a,b]);
+        this.array.push([a,b]);
     }
   
     // the only thing needed to move is the angle
     move(angle)
     {
-      var a=this.array[this.array.length-2];
-      var b=this.array[this.array.length-1];
-      var vectorx=(b[0]-a[0])
-      var vectory=(b[1]-a[1])
-      // we take the vector of the previous move
-  
-      // we apply a rotation matrix
-      var x=vectorx*Math.cos(angle)-vectory*Math.sin(angle)
-      var y=vectorx*Math.sin(angle)+vectory*Math.cos(angle)
-  
-      // and add this new vector to the last coord
-      var new_x=b[0]+x
-      var new_y=b[1]+y
-  
-      // print("distance",((new_x-b[0])**2+(new_y-b[1])**2)**0.5)
-      if(Line.interruption === false) this.array.push([new_x,new_y,'player']);
-      else this.array.push([new_x,new_y,'empty']);
+        if(this.array.length % 200 === 0){
+            //clearInterval(this.interruptionInterval);
+            //this.interruptionInterval = setInterval(this.randomInterval.bind(this), Math.random() * 1000 + 1000);
+        }
+
+        if(this.colorCounter === 100){
+            this.colorCounterMode = "decrement";
+        }
+        else if(this.colorCounter === 0){
+            this.colorCounterMode  = "increment";
+        }
+
+        if(this.colorCounterMode === "decrement") this.colorCounter--;
+        else if(this.colorCounterMode === "increment") this.colorCounter++;
+
+        this.currentColor = lerpColor(currentFromColor, currentToColor, (this.colorCounter) / 100.0);
+        //this.currentColor = color(255,255,255);
+        var a=this.array[this.array.length-2];
+        var b=this.array[this.array.length-1];
+        var vectorx=(b[0]-a[0])
+        var vectory=(b[1]-a[1])
+        // we take the vector of the previous move
+    
+        // we apply a rotation matrix
+        var x=vectorx*Math.cos(angle)-vectory*Math.sin(angle)
+        var y=vectorx*Math.sin(angle)+vectory*Math.cos(angle)
+    
+        // and add this new vector to the last coord
+        var new_x=b[0]+x
+        var new_y=b[1]+y
+    
+        // print("distance",((new_x-b[0])**2+(new_y-b[1])**2)**0.5)
+        this.head = [new_x,new_y];
+        if(this.interruption === false) this.array.push([new_x,new_y,'player']);
+        else this.array.push([new_x,new_y,'empty']);
     }
   
     // we only display the last line
     display()
     {
-      for(var i=this.array.length-2;i<this.array.length-1;i++)
-      {
-        line(this.array[i][0],this.array[i][1],this.array[i+1][0],this.array[i+1][1]);
-      }
+        for(var i=this.array.length-10;i<this.array.length-1;i++)
+        {
+            if(this.array[i]){
+                if(this.array[i][2] === 'player' && this.array[i+1][2] === 'player') {
+                    line(this.array[i][0],this.array[i][1],this.array[i+1][0],this.array[i+1][1]);
+                }
+                else {
+                    drawingContext.shadowBlur = 0;
+                    drawingContext.shadowColor = color(0,0,0,0);
+                }
+            }
+        }
+
+        if(bloom === true){
+            drawingContext.shadowBlur = 32;
+            drawingContext.shadowColor = this.currentColor;
+        }
+        line(this.head[0],this.head[1],this.head[0],this.head[1]);
     }
   
     // display line foreach player
-    static display_lines(player_lines){
-        if(!Line.interruptionInterval)Line.interruptionInterval = setInterval(Line.randomInterval, 2000);
-        var linesToRemove = [];
-        for (var i=0;i<player_lines.length;i++)
-        {
-            console.log(Line.interruption);
-            if(Line.interruption === true){
-                linesToRemove.push(i);
-                
-            } else {
-                stroke(playerColors[i]);
-                strokeWeight(lineWidth);
-                player_lines[i].display();
-            }
-        }
+    display_lines(line){
+        stroke(this.currentColor);
+        strokeWeight(lineWidth);
+        drawingContext.shadowBlur = 0;
+        drawingContext.shadowColor = color(0,0,0,0);
+        line.display();
+    }
+
+    resetHeadEnvironment(){
+        fill(0,0,0);
+        stroke(0,0,0,0);
+        drawingContext.shadowBlur = 0;
+        drawingContext.shadowColor = color(0,0,0,0);
+        ellipse(this.head[0], this.head[1], 22,22);
     }
   
     // check if the game is ended
