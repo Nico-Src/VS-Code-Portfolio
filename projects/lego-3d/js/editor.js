a6ced8568b2fe95a4d3fde87d5ec93302cb568e0
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2022-08-27 17:20:20 +0200

    Brick Colors can be changed + Hotkeys added

--- projects/lego-3d/js/editor.js
+++ projects/lego-3d/js/editor.js
@@ -22,13 +22,19 @@ class Editor{
         this.gridEnabled = !this.config.grid.show; // because it is toggled on startup it will be reversed
         this.canvas = canvas;
         this.elements = [];
+        this.keys = new Map();
+        this.setupKeys();
         this.init();
+    }
+
+    setupKeys(){
+        this.keys.set('ControlLeft', false);
     }
 
     init(){
         this.engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
         this.scene = new BABYLON.Scene(this.engine); // Generate the scene
-        this.scene.clearColor = new BABYLON.Color4(0.2,0.2,0.2,1); // Set a background color
+        this.scene.clearColor = new BABYLON.Color4(0.23,0.247,0.286,1); // Set a background color
         this.camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), this.scene); // Generate the camera
         this.camera.wheelPrecision = this.config.camera.zoomSpeed;
         this.camera.zoomToMouseLocation = this.config.camera.zoomToMouseLocation;
@@ -39,17 +45,16 @@ class Editor{
         this.scene.ambientColor = new BABYLON.Color3(0.2, 0.2, 0.2); // Set the ambient color of the scene to dark gray
         
         const lights = [];
-        lights.push(new BABYLON.PointLight("light1", new BABYLON.Vector3(5, 10, 5), this.scene)); // Generate a light
-        lights.push(new BABYLON.PointLight("light1", new BABYLON.Vector3(-5, 10, 5), this.scene)); // Generate a light
-        lights.push(new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 10, -5), this.scene)); // Generate a light
+        lights.push(new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(5, 5, 10), this.scene)); // Generate a light
 
         lights.forEach(light=>{
-            light.intensity = 0.35;
+            light.intensity = 0.5;
+            light.groundColor = new BABYLON.Color3(0.2, 0.2, 0.2);
         });
 
         const cube = BABYLON.MeshBuilder.CreateBox("box", {}, this.scene);
         cube.position.x = 5000;
-        this.addElement(this.getUniqueId(), "box", "box", "active", cube);
+        this.addElement(this.getUniqueId(), "box", "box", false, cube);
 
         this.scene.getBoundingBoxRenderer().frontColor.set(.46, 0.96, 0.96);
         this.scene.getBoundingBoxRenderer().backColor.set(.46, 0.96, 0.96);
@@ -83,6 +88,21 @@ class Editor{
         this.assetsManager.onFinish = (tasks)=>{
             this.engine.runRenderLoop(()=>{
                 this.scene.render();
+
+                this.elements.forEach(el=>{
+                    if(el.type === 'brick' && el.selected){
+                        el.element.showBoundingBox = true;
+                        el.element.enableEdgesRendering();
+                        el.element.edgesWidth = this.config.selection.lineWidth;
+                        el.element.edgesColor = this.config.selection.lineColor;
+                    } else if(el.type === 'brick' && el.hovering) {
+                        el.element.showBoundingBox = true;
+                        el.element.disableEdgesRendering();
+                    } else if(el.type === 'brick') {
+                        el.element.showBoundingBox = false;
+                        el.element.disableEdgesRendering();
+                    }
+                });
             });
             
             // resize engine when window is resized
@@ -101,8 +121,35 @@ class Editor{
         document.onkeydown = (e) => {
             if(e.keyCode === 71){
                 this.toggleGrid();
+            } else if(e.code === 'KeyA' && e.ctrlKey){
+                e.preventDefault();
+                this.elements.forEach(el=>{
+                    if(el.type === 'brick'){
+                        el.selected = true;
+                    }
+                });
+            } else if(e.code === 'KeyA' && e.shiftKey){
+                e.preventDefault();
+                this.elements.forEach(el=>{
+                    if(el.type === 'brick'){
+                        el.selected = false;
+                    }
+                });
+            } else if(e.code === 'Delete'){
+                let selectedElements = this.elements.filter(el=>el.selected);
+                selectedElements.forEach(el=>{
+                    const index = this.elements.indexOf(el);
+                    this.elements[index].element.dispose();
+                    this.elements.splice(index, 1);
+                });
             }
+
+            this.keys.set(e.code, true);
         };
+
+        document.onkeyup = (e) => {
+            this.keys.set(e.code, false);
+        }
 
         // on pointer down
         this.scene.onPointerDown = (evt, pickInfo) => {
@@ -227,6 +274,7 @@ class Editor{
             name: name,
             type: type,
             selected: selected,
+            hovering: false,
             element: element,
         });
     }
@@ -244,8 +292,12 @@ class Editor{
         brick.position.z = 0;
         // create lego brick material and set it to the brick
         const brickMaterial = new BABYLON.StandardMaterial("mat", this.scene);
-        brickMaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
-        brickMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        const r = this.currentColor.rgba[0] / 255;
+        const g = this.currentColor.rgba[1] / 255;
+        const b = this.currentColor.rgba[2] / 255;
+        brickMaterial.diffuseColor = new BABYLON.Color3(r, g, b);
+        brickMaterial.alpha = this.currentColor.rgba[3];
+        brickMaterial.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
         brickMaterial.emissiveColor = new BABYLON.Color3(0, 0, 0);
         brickMaterial.ambientColor = new BABYLON.Color3(0.75, 0.75, 0.75);
         brick.material = brickMaterial;
@@ -266,6 +318,7 @@ class Editor{
             data: brickData,
             type: 'brick',
             selected: false,
+            hovering: false,
         });
     }
 
@@ -276,8 +329,17 @@ class Editor{
         // get new active element
         this.elements.forEach((object)=>{
             // check if the picked mesh is the same as the current elements mesh
-            if(object.element === pickedMesh && object.type !== "light") object.selected = !object.selected;
-            else object.selected = false;
+            if(object.element === pickedMesh && object.type === "brick"){
+                // TODO check if strg is pressed so multiple can be selected
+                const isSelected = object.selected;
+                if(this.keys.get('ControlLeft') === false){
+                    console.log("Deselting all");
+                    this.elements.forEach(el => el.selected = false);
+                }
+                object.selected = !isSelected;
+                document.dispatchEvent(new CustomEvent('selection-changed', { detail: {selectedBricks: this.elements.filter(el => el.type === 'brick' && el.selected === true)} }));
+            }
+            else if(this.keys.get('ControlLeft') === false) object.selected = false;
         });
     }
 
@@ -290,30 +352,12 @@ class Editor{
             hoveringMesh = brickInfo.pickedMesh;
         }
 
-        // if not brick is hovered hide all bounding boxes
-        if(!hoveringMesh){
-            this.elements.forEach(el=>{
-                if(el.type === 'brick'){
-                    el.element.showBoundingBox = false;
-                    el.element.disableEdgesRendering();
-                }
-            });
-            return;
-        }
-
-        // enable bounding box of the hovered brick
-        if(hoveringMesh.showBoundingBox !== true){
-            hoveringMesh.showBoundingBox = true;
-            hoveringMesh.enableEdgesRendering();	
-            hoveringMesh.edgesWidth = this.config.selection.lineWidth;
-            hoveringMesh.edgesColor = this.config.selection.lineColor;
-        }
-
         // hide all other bounding boxes
         this.elements.forEach(el=>{
-            if(el.element !== hoveringMesh && el.type === 'brick'){
-                el.element.showBoundingBox = false;
-                el.element.disableEdgesRendering();
+            if(el.element === hoveringMesh && el.type === 'brick'){
+                el.hovering = true;
+            } else {
+                el.hovering = false;
             }
         });
     }
@@ -325,9 +369,12 @@ class Editor{
         // Use a predicate to get position on the ground
         var pickInfo = this.scene.pick(this.scene.pointerX, this.scene.pointerY, (mesh) => { return mesh == this.grid; });
         var brickInfo = this.scene.pick(this.scene.pointerX, this.scene.pointerY, (mesh) => { return mesh != this.grid && mesh != this.currentMesh && mesh.isPickable === true });
+        // check if brick is dragged onto another brick
         if (brickInfo.hit) {
-            brickInfo.pickedMesh._position_y += .175 - this.grid.position._y; // replace with brick height later
-            return new BABYLON.Vector3(brickInfo.pickedPoint.x, brickInfo.pickedMesh._position._y + 0.235, brickInfo.pickedPoint.z);
+            // get brick info
+            const brick = BrickLib.bricks.find(b=>b.name === brickInfo.pickedMesh.name);
+            // set brick y position based on the height of the brick it is place on
+            return new BABYLON.Vector3(brickInfo.pickedPoint.x, brickInfo.pickedMesh._position._y + brick.height, brickInfo.pickedPoint.z);
         } else if(pickInfo.hit){
             pickInfo.pickedPoint.y -= this.grid.position._y; // add grid offset to make brick position correct
             return pickInfo.pickedPoint;
