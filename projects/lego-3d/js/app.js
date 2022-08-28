ea2ffad98519d2bb797df78b1af0d8576642af21
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2022-08-28 18:03:10 +0200

    Top-Bar Navigation Items click handler added

--- projects/lego-3d/js/app.js
+++ projects/lego-3d/js/app.js
@@ -2,3 +2,15 @@ const canvas = document.getElementById("editor");
 const editor = new Editor(canvas);
 const toolbox = new ToolBox(editor);
 const inspector = new Inspector(editor);
+
+Array.from(document.querySelectorAll('.top-bar .nav-item')).forEach(navItem=>{
+    Array.from(navItem.querySelectorAll('.dropdown-item')).forEach(dropdownItem=>{
+        dropdownItem.onclick = () => {
+            editor[dropdownItem.getAttribute('data-action')]();
+            navItem.classList.add('disabled');
+            setTimeout(() => {
+                navItem.classList.remove('disabled');
+            }, 350);
+        };
+    });
+});
