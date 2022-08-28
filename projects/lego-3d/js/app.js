4ccfab84ee2f402dd48db79be91a9b0ab7fac042
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2022-08-28 18:11:39 +0200

    Navbar updated

--- projects/lego-3d/js/app.js
+++ projects/lego-3d/js/app.js
@@ -3,14 +3,29 @@ const editor = new Editor(canvas);
 const toolbox = new ToolBox(editor);
 const inspector = new Inspector(editor);
 
+document.onclick = (e) => {
+    console.log(e);
+    if(!e.target.classList.contains('.dropdown') && !e.target.classList.contains('.dropdown-item') && !e.target.classList.contains('.nav-item')){
+        Array.from(document.querySelectorAll('.nav-item')).forEach(nav => nav.classList.remove('expanded'));
+    }
+};
+
 Array.from(document.querySelectorAll('.top-bar .nav-item')).forEach(navItem=>{
+    navItem.onclick = (e) => {
+        e.stopPropagation();
+        Array.from(document.querySelectorAll('.top-bar .nav-item')).forEach(nav => {
+            if(nav !== navItem){
+                nav.classList.remove('expanded');
+            }
+        });
+        navItem.classList.toggle('expanded');
+    }
     Array.from(navItem.querySelectorAll('.dropdown-item')).forEach(dropdownItem=>{
-        dropdownItem.onclick = () => {
+        dropdownItem.onclick = (e) => {
+            e.stopPropagation();
+            // call editor function for dropdown item
             editor[dropdownItem.getAttribute('data-action')]();
-            navItem.classList.add('disabled');
-            setTimeout(() => {
-                navItem.classList.remove('disabled');
-            }, 350);
+            navItem.classList.remove('expanded');
         };
     });
 });
