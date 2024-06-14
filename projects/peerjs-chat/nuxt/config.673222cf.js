7159ab55bf6bca083482da476f3de32ffbb42ff6
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2024-06-14 18:03:27 +0200

    System messages styled differently now

---
+++ projects/peerjs-chat/nuxt/config.673222cf.js
@@ -0,0 +1,1 @@
+import{ah as p,ai as i,l as o}from"./entry.f613d0fb.js";const a={nuxt:{}},t=p(a);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
--- projects/peerjs-chat/nuxt/config.8a51bff4.js
+++
@@ -1,1 +0,0 @@
-import{ah as p,ai as i,l as o}from"./entry.edba93fc.js";const a={nuxt:{}},t=p(a);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
