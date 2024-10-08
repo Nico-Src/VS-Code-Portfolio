9f7c4b0a2f8aeb1fa8fad09f9a55a4449e9098a6
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2024-10-08 20:57:46 +0200

    New Windows 11 Version

---
+++ projects/windows-11/nuxt/config.28da6ab3.js
@@ -0,0 +1,1 @@
+import{aw as p,ax as o,l as i}from"./entry.333374e9.js";const a={nuxt:{}},t=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
--- projects/windows-11/nuxt/config.3f7ce7f5.js
+++
@@ -1,1 +0,0 @@
-import{aw as p,ax as o,l as i}from"./entry.169eea64.js";const a={nuxt:{}},t=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
